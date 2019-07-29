0618aeca061dae59260f56c55a0772a840997005
whvcse@foxmail.com <w1114521>
2019-07-29 19:16:21 +0800

    解决跨域options请求问题

--- src/main/java/com/wf/ew/common/config/MyWebMvcConfigurer.java
+++ src/main/java/com/wf/ew/common/config/MyWebMvcConfigurer.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.filter.HttpPutFormContentFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
@@ -24,4 +25,8 @@ public class MyWebMvcConfigurer implements WebMvcConfigurer {
         return new HttpPutFormContentFilter();
     }
 
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new OptionsInterceptor()).addPathPatterns("/**");
+    }
 }
